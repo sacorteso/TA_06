e1ca629f4084cb380c9e696f4d2ced1b4da72145
JUAN CARLOS VELÁSQUEZ GUTIÉRREZ <80866035+jucvelasquezgu@users.noreply.github.com>
2021-04-15 09:10:50 -0500

    Update TA_06_info03.py

--- TA_06_info03.py
+++ TA_06_info03.py
@@ -2,9 +2,22 @@
 
 k=input("ingrese su clave: ")
 L="administrador"
+Tnombre=[]
+Tapellido=[]
+Tclave=[]
+total=[]
 while(L==k):
-      for i in range(3)
+      for i in range(3):
+        nombre=input("ingrese su nombre: ")
+        Tnombre=nombre
+        apellido=input("ingrese su apellido: ")
+        Tapellido=apellido
+        clave= input("ingrese su clave: ")
+        Tclave= clave
+        
+
         print(i)
       break
+print(total)      
       
     
