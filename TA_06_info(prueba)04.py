196f19bdc8bfd5b38063f4c4c83d44cb5ba07cab
santiago cortes <>
2021-04-22 15:03:35 +0000

    modificacion del formato.pyw a .py

---
+++ TA_06_info(prueba)04.py
@@ -0,0 +1,22 @@
+
+
+# interfaz, usuario con clave
+#Página de ingreso de productos
+#dataframe bodega
+#pagina de compras
+#dataframe compras
+#pip install pandas
+#pip install Numpy
+#import pandas as pd
+#import Numpy as pn
+bodegaf = {'distribuidor':['cocacola','postobon'],'productos':['delvalle','manzana']}
+#clase=['granos','bebidas','lacteos','limpieza','mekatos','panadera']
+#producto=['naranja','Mango','Banano','fresa']
+#empresa=[]
+#distribuidor= []
+#Precio=[]
+#Fecha=[]
+#codigo=[]
+#Bodega= pd.DataFrame['clase','producto']
+df = pd.DataFrame(bodegaf)
+print(df)
