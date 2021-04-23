ce077099bb82e4dcdee209cecb4904fba8689df1
sacorteso <80845997+sacorteso@users.noreply.github.com>
2021-04-23 13:17:29 -0500

    Update TA_06_info04.py

--- TA_06_info04.py
+++ TA_06_info04.py
@@ -152,52 +152,7 @@ while True:
             #print("")
             #input("No has elegido ningún valor correcto...\n pulsa enter para continuar")
 '''
-'''def ingresoproductosbodega():
-  while True:
-     ig=1
-     #listas definidas para las columnas
-     distribuidor=[]
-     producto=[]
-     referencia=[]
-     precio=[]
-     unidad=[]
-     valorxunidad=[]
-     fecompra=[]
-     for i in range(ig):
-      #ingreso de los productos para la bodega
-       distribuidor1=input("\ningrese el nombre del distribuidor: ")
-       producto2= input("\ningrese el nombre del producto: ")
-       refproducto2= input("\ningrese la referencia del producto: ")
-       precio2= input("\ningrese el precio de la paca: ")
-       unidad2= input("\ningrese la cantidad de unidades de la paca: ")
-       fecompra1= input("\ningrese la fecha de compra: ")
-       vxunidad = int (precio2) / int (unidad2)
-
-      
-
-       distribuidor.append(distribuidor1)
-       producto.append(producto2)
-       referencia.append(refproducto2)
-       precio.append(precio2)
-       unidad.append(unidad2)
-       fecompra.append(fecompra1)
-       valorxunidad.append(vxunidad)
-
-
-
-       bodega1=(distribuidor, producto, referencia, precio, unidad, valorxunidad,fecompra )
-       os.system('clear')
-       print("\t\t\t\t PÁGINA DE BODEGA \n\n")
-       df1= pd.DataFrame(bodega1)
-       print("\t\t PRODUCTOS EN BODEGA \n\n")
-       df2=pd.DataFrame.transpose(df1)
-       df2.columns=['distribuidor', 'producto','referencia', 'precio', 'unidad', 'valorxunidad', 'fecompra']
-       print(df2)
-       df2.to_csv('bodega1.csv', mode="a", index="", header="")
-     else:
-       break
-
-
+'''
 print("¿deseas infresar productos en bodega?")
 input("presiona enter para continuar:")
 ingresoproductosbodega()
