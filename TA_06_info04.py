8c86abeb535d73ca2ff4c91b944bbc55b25572b6
santiago cortes <>
2021-04-23 18:11:10 +0000

    se modifica el código para que corra perfectamente

--- TA_06_info04.py
+++ TA_06_info04.py
@@ -12,6 +12,51 @@ import os
 import csv
 
 
+def ingresoproductosbodega():
+  while True:
+     ig=1
+     #listas definidas para las columnas
+     distribuidor=[]
+     producto=[]
+     referencia=[]
+     precio=[]
+     unidad=[]
+     valorxunidad=[]
+     fecompra=[]
+     for i in range(ig):
+      #ingreso de los productos para la bodega
+       distribuidor1=input("\ningrese el nombre del distribuidor: ")
+       producto2= input("\ningrese el nombre del producto: ")
+       refproducto2= input("\ningrese la referencia del producto: ")
+       precio2= input("\ningrese el precio de la paca: ")
+       unidad2= input("\ningrese la cantidad de unidades de la paca: ")
+       fecompra1= input("\ningrese la fecha de compra: ")
+       vxunidad = int (precio2) / int (unidad2)
+
+      
+
+       distribuidor.append(distribuidor1)
+       producto.append(producto2)
+       referencia.append(refproducto2)
+       precio.append(precio2)
+       unidad.append(unidad2)
+       fecompra.append(fecompra1)
+       valorxunidad.append(vxunidad)
+
+
+
+       bodega1=(distribuidor, producto, referencia, precio, unidad, valorxunidad,fecompra )
+       os.system('clear')
+       print("\t\t\t\t PÁGINA DE BODEGA \n\n")
+       df1= pd.DataFrame(bodega1)
+       print("\t\t PRODUCTOS EN BODEGA \n\n")
+       df2=pd.DataFrame.transpose(df1)
+       df2.columns=['distribuidor', 'producto','referencia', 'precio', 'unidad', 'valorxunidad', 'fecompra']
+       print(df2)
+       df2.to_csv('bodega1.csv', mode="a", index="", header="")
+     else:
+       break
+
 # función para crear la pnatalla de compras e ingreso de productos
 def compra():
    while True:
@@ -69,7 +114,7 @@ def menu():
     print("Selecciona una opción \n")
     print("\t1 - Registro de compras ")
     print("\t2 - registro total acumulado")
-    print("\t3 - Otras opciones")
+    print("\t3 - ingreso de bodega")
     print("\t9 - Terminar con uso de menu")
           
 # Se crea un una iteración con while mientras se cumpla la condición
@@ -87,65 +132,14 @@ while True:
         
       elif opcionMenu == "2":
              total()
-      else:                     
-       break
+      #else:                     
+       #break
 
       elif opcionMenu == "3":
-
-              print("")
-              input("\nSe ha pulsado la opción 2...\n pulsa enter para continuar")
-              def ingresoproductosbodega():
-  while True:
-     ig=1
-     #listas definidas para las columnas
-     distribuidor=[]
-     producto=[]
-     referencia=[]
-     precio=[]
-     unidad=[]
-     valorxunidad=[]
-     fecompra=[]
-     for i in range(ig):
-      #ingreso de los productos para la bodega
-       distribuidor1=input("\ningrese el nombre del distribuidor: ")
-       producto2= input("\ningrese el nombre del producto: ")
-       refproducto2= input("\ningrese la referencia del producto: ")
-       precio2= input("\ningrese el precio de la paca: ")
-       unidad2= input("\ningrese la cantidad de unidades de la paca: ")
-       fecompra1= input("\ningrese la fecha de compra: ")
-       vxunidad = int (precio2) / int (unidad2)
-
-      
-
-       distribuidor.append(distribuidor1)
-       producto.append(producto2)
-       referencia.append(refproducto2)
-       precio.append(precio2)
-       unidad.append(unidad2)
-       fecompra.append(fecompra1)
-       valorxunidad.append(vxunidad)
-
-
-
-       bodega1=(distribuidor, producto, referencia, precio, unidad, valorxunidad,fecompra )
-       os.system('clear')
-       print("\t\t\t\t PÁGINA DE BODEGA \n\n")
-       df1= pd.DataFrame(bodega1)
-       print("\t\t PRODUCTOS EN BODEGA \n\n")
-       df2=pd.DataFrame.transpose(df1)
-       df2.columns=['distribuidor', 'producto','referencia', 'precio', 'unidad', 'valorxunidad', 'fecompra']
-       print(df2)
-       df2.to_csv('bodega1.csv', mode="a", index="", header="")
-     else:
-       break
-
-
-print("¿deseas infresar productos en bodega?")
-input("presiona enter para continuar:")
-ingresoproductosbodega()
-
-       else:
-         break
+             ingresoproductosbodega()
+        
+      else:
+        break
             
 
 '''
