496933d09d12ce55f2e3a565b307b3f206e19326
JUAN CARLOS VELÁSQUEZ GUTIÉRREZ <>
2021-05-07 07:14:02 +0000

    Se crea base de datos y algoritmos para registrar el valor total de la compra  con fecha y hora, además se coloca la explicación de cada línea del script para ventas.

--- TA_06_info06.py
+++ TA_06_info06.py
@@ -1,18 +1,20 @@
-from datetime import date as dt
-from datetime import datetime as dtt
-from datetime import timedelta
+from datetime import date as dt #librería para maejar fechas
+from datetime import datetime as dtt  #librería para manejar el tiempo
+from datetime import timedelta        #librearía para realizar operaciones matemáticas con el tiempo
 import pandas as pd # Librería para utilizar y manipular dataframe
 import numpy as np  # Librería para realizar operaciones matemáticas y manejo de matrices
 import os           # Librerías para realizar funciones internas del software
-import csv 
-import time
-today=dt.today()
-#print(today)
-f5=today.strftime('%Y%m%d')
-f55=int(f5)
-print(f55)
-f56=time.strftime('%H:%M:%S')
-print(f56)
+import csv          # Librería para manipular archivos con extensión csv
+import time         # Librería para manipular el tiempo
+
+
+
+today=dt.today()            # comando que muestra la fecha actual
+f5=today.strftime('%Y%m%d') # para cambiar el formato de fecha y unirlo
+f55=int(f5)                 # para convertir la fecha en un entero para operciones matemáticas
+print(f55)                  # para imprimir la hora
+f56=time.strftime('%H:%M:%S') # pra modificar el foramto de la hora
+print(f56)                    # para imprimir la hora
 
 
 ce=[] # Se crea lista para almacenar la identificación del cliente
@@ -23,68 +25,79 @@ c4=[] # Se crea lista para almacenar la cantidad de productos que se va a compra
 c5=[] # Se crea lista para almacenar el subtotatal de la compra por referencia de producto
 c6=[] # Se crea lista para almacenar la fecha
 c7=[] # Se crea lista para almacenar la hora
-cc= input("\n\nIngrese la identificación del cliente: ")
-cc=int(cc)
 
-z1= input("\n\ningrese la cantidad de productos que se van a vender: ")
-z=int(z1)
-zz1=0
-zz2=0
-subtotal=0
+cc= input("\n\nIngrese la identificación del cliente: ") # ingreso de la identificación del cliente
+cc=int(cc)                 # se convierte el valor de la identificación en un entero
 
-subtotal=[]
-for k in range(z):
-  print(k)
-  L6 =pd.read_csv("BDATOS/mostrador.csv") 
-  print(L6.iloc[:,0:2])
-  Ref1= input("\n\nIngrese la referencia del producto: ")
-  #Ref= str(Ref1)
-  can= input("\n\nIngrese la cantidad de producto:  ")
-  can= int(can)
-  for kk in range(len(L6)):
-    if(L6.iloc[kk,1]== Ref1):
-      zz1= L6.iloc[kk,0]
-      zz2= L6.iloc[kk,6]
-      subtotal= zz2*can
+z1= input("\n\ningrese la cantidad de productos que se van a vender: ") # para crear un ciclo en donde se estima la cantidad de productos que hay que registrar
+z=int(z1)                  # Se convierte el valor del ciclo en un entero
+zz1=0                      # para generar variable para el nombre
+zz2=0                      # para generar variable para el precio individual del producto
+subtotal=[]                # para geneerar variables del valor subtotal ed la venta
 
-      ce.append(cc)
-      c1.append(Ref1)
-      c2.append(zz1)
-      c3.append(zz2)
-      c4.append(can)
-      c5.append(subtotal)
-      c6.append(f55)
-      c7.append(f56)
+
+for k in range(z):         # se crea una iteración para registrar las los productos que se van a vender
+  L6 =pd.read_csv("BDATOS/mostrador.csv") # SE lee la base de datos mostrador
+  print(L6.iloc[:,0:2])    # se imprime el nombre y a referencia del producto
+  Ref1= input("\n\nIngrese la referencia del producto: ") # para ingresar la respectiva referencia del producto
+  
+  can= input("\n\nIngrese la cantidad de producto:  ") # Para ingresar la cantidad de productos que se van a comprar de la respectiva referencia
+  can= int(can)               # para convertir en entero la cantidad de productos que se van a comprar de la respectiva referencia 
+  for kk in range(len(L6)):   # se crea una interación para revisar las refrencias de los productos
+    if(L6.iloc[kk,1]== Ref1): # SE activa la condición de que los productos tengan la misma referencia del producto que va a comprar el cliente
+      zz1= L6.iloc[kk,0]      # se busca el nombre del producto
+      zz2= L6.iloc[kk,6]      # Se busca el precio unitario del producto
+      subtotal= zz2*can       # se halla el subtotal de la compra de ese producto
+
+      ce.append(cc)           # Se anexa a lista para almacenar la identificación del cliente
+      c1.append(Ref1)         # Se anexa a lista para almacenar la referencia del producto
+      c2.append(zz1)          # Se anexa a lista para almacenar el nombre del producto
+      c3.append(zz2)          # Se anexa a lista para almacenar el valor unitario del producto
+      c4.append(can)          # Se anexa a lista para almacenar la cantidad de productos que se va a comprar un cliente respecto a una referencia determinada
+      c5.append(subtotal)     # Se anexa a lista para almacenar el subtotatal de la compra por referencia de producto
+      c6.append(f55)          # Se anexa a lista para almacenar la fecha
+      c7.append(f56)          # Se anexa a lista para almacenar la hora
       
-      factura=(ce,c1,c2,c3,c4,c5,c6,c7)
-      factura1=pd.DataFrame(factura)
-      factura2=pd.DataFrame.transpose(factura1)
+      factura=(ce,c1,c2,c3,c4,c5,c6,c7) # se crea lista con los valores de las respectivas variables
+      factura1=pd.DataFrame(factura)    # Se crea DataFrame para la factura
+      factura2=pd.DataFrame.transpose(factura1) # Se transpone el Dataframe factura
+      
+      # Se coloca los nombres a las columnas
       factura2.columns=['Cedula','Referencia','Nproducto','Punitario','Cantidad','Subtotal','Fecha','Hora']
       
-      temporal=pd.DataFrame.transpose(factura1)
+      temporal=pd.DataFrame.transpose(factura1)  # Se crea el DataFrame temporal que va permitir crear la factura
+      
+      # Se coloca los nombres a las columnas
       temporal.columns=['Cedula','Referencia','Nproducto','Punitario','Cantidad','Subtotal','Fecha','Hora']
       
+      # Se graban los datos para la factura en la base de datos temporal
       temporal.to_csv('BDATOS/temporal.csv', mode="a", index="", header="")
       
-
-
+      # Se graban los datos para la factura en la base de datos factura para consultas en el futuro
       factura2.to_csv('BDATOS/factura.csv', mode="a", index="", header="")
   
-temporal22=pd.read_csv('BDATOS/factura.csv')
-  #print(temporal22)
-factura22=pd.read_csv('BDATOS/factura.csv')
-  #print(factura22)
-Vtotal=temporal22['Subtotal'].sum()
-tem=temporal22.drop(['Fecha','Hora'], axis=1)
-c1=cc
-os.system('clear') 
-print("\n\n\n\n\t\t\t\t\t                  FACTURA DE VENTA")
-print("\n\n\n\n")
-print("Cédula: ",c1, "\t\t\t\t\tFecha: " ,       f55,   "\t\t\t\t\tHora: ",        f56)
-print("\n\n\n\n")
-print(tem)
-print("\n\n")
-print("\n\n\t\t           Valor total de la venta: ", Vtotal)
+temporal22=pd.read_csv('BDATOS/factura.csv')  # Para leer la base de datos temporal
+  
+factura22=pd.read_csv('BDATOS/factura.csv')   # Para leer la base de datos de factura
+  
+Vtotal=temporal22['Subtotal'].sum()  # para sumar el valor total de la compra de productos
+tem=temporal22.drop(['Fecha','Hora'], axis=1) # Para eliminar columnas de la fecha y hora
+c1=cc                                # variable para la identificcaión del cliente
+c2= Vtotal                           # variable para el valor total de la compra
+os.system('clear')                         # para limpiar la pantalla
+print("\n\n\n\n\t\t\t\t\t                  FACTURA DE VENTA") # titulo de la página factura de venta
+print("\n\n\n\n")                          #salto de linea                    
+print("Cédula: ",c1, "\t\t\t\t\tFecha: " ,       f55,   "\t\t\t\t\tHora: ",        f56) # para insertar cédula, fecha , hora
+print("\n\n\n\n")                          # salto de línea
+print(tem)                                 # Se imprime datos de la compra para la factura
+print("\n\n")                              # salto de linea
+print("\n\n\t\t           Valor total de la venta: ", Vtotal)  # se imprime el valor total de la venta
 
 
-
+ventas=(c1,c2,f55,f56)                     # Se crea lista para ventas totales
+ventas1=pd.DataFrame(ventas)               # Se crea dataframe para ventas totales
+ventas2=pd.DataFrame.transpose(ventas1)    # se transpone el dataframe para organizar los datos
+ventas2.columns=['Cedula','Vtotal','Fecha','Hora'] # se coloca nombres a las columnas
+ventas2.to_csv('BDATOS/ventas.csv', mode="a", index="", header="") # Se graba los datos para ventas totales en el archivo ventas.csv
+ventas2= pd.read_csv('BDATOS/ventas.csv')  # para leer el contenido del archivo de ventas totales
+print("\n\n\n\n",ventas2)                  # se imprime el contenido del archivo de ventas totales
