a06e67782170b96fa427fe136e41ae5b66e87f99
JUAN CARLOS VELÁSQUEZ GUTIÉRREZ <>
2021-05-14 15:42:11 +0000

    Se añade explicación a las filas del script de la función contabilidad

--- TA_06_info07.py
+++ TA_06_info07.py
@@ -280,24 +280,24 @@ def contabilidad():       # se crea la función que controla toda la contabilida
 
 
 
-    ing=pd.read_csv("BDATOS/ventas.csv")
-    print(ing)
+    ing=pd.read_csv("BDATOS/ventas.csv")    # Se lee la base de datos de ventas 
+    print(ing)                              # Se imprime la base de datos de ventas
 
 
 
-    ing2= ing[(ing['Fecha']==IF)]
-    ing3=pd.DataFrame(ing2)
+    ing2= ing[(ing['Fecha']==IF)]           # En la base de datos de ventas se busca la información con referencia a una fecha especifica
+    ing3=pd.DataFrame(ing2)                 # Se crea un nuevo DataFrame con los datos encontardos anteriormente
 
 
-    ing5=ing3['Vtotal'].sum()    # Valor en dinero de ventas entre un intervalo de tiempo
+    ing5=ing3['Vtotal'].sum()               # Se suman  el valor en dinero de todas las ventas entre un intervalo de tiempo
 
 
-    cont2=pd.read_csv("BDATOS/contabilidad1.csv")
-    kq=len(cont2)
-    print(kq)
-    capbase=cont2.iloc[kq-1,1]
-    aux1=cont2.iloc[kq-1,5]
-    porc=cont2.iloc[kq-1,6]
+    cont2=pd.read_csv("BDATOS/contabilidad1.csv")  # Se lee la base de datos de contabilidad
+    kq=len(cont2)                                  # se halla la cantidad de filas y se convierte en una constante
+    print(kq)                                      # Se imprime la cantidad de filas
+    capbase=cont2.iloc[kq-1,1]                     # SE encuentra la última fila de la base de datos para la fecha
+    aux1=cont2.iloc[kq-1,5]                        # Se encuentra la ultima fila de la base de datos para entradas de dinero extra
+    porc=cont2.iloc[kq-1,6]                        # se encuentra la ultima fila para el porcentaje de ganancia
     ganancianeta= ing5*porc/100     # operación matemática para hallar la ganancia diaria
 
     aux=ing5-ganancianeta           # operación matemática para extraer el capital para reinvertir
@@ -306,18 +306,18 @@ def contabilidad():       # se crea la función que controla toda la contabilida
     Disponible= cap-F5-TotalCFF     # Se resta al capital los costos de compras de productos y costos fijos de funcionamiento diarios
     cap= Disponible                 # Se actualiza el capital base del negocio
 
-    cont=[]
-    cont=[IF,cap,F5,TotalCFF,ing5,Ent,porc,ganancianeta,Disponible]
+    cont=[]                         # Se crea una lista
+    cont=[IF,cap,F5,TotalCFF,ing5,Ent,porc,ganancianeta,Disponible]  # Se ingresa a las lista la fecha seleccionada, capital base, compras en bodega,
+                                                                     # costos fijos, entrada de dinero extra, porcentaje de ganancia, Dinero disponibe después de pagar facturas y costos
 
-
-    cont1=pd.DataFrame(cont)
-    cont2=pd.DataFrame.transpose(cont1)
-
+    cont1=pd.DataFrame(cont)                                         # Se crea un nuevo DataFrame con los valores anteriores
+    cont2=pd.DataFrame.transpose(cont1)                              # Se transpone el DataFrame anterior
+    # Se coloca nombre a  las columnas del nuevo DataFrame
     cont2.columns=['IF','capitalbase','CBodega','Cfijos','Ventas','EntradaExtra','porc','ganancianeta','Disponible']
-    cont2.to_csv("BDATOS/contabilidad1.csv", mode="a", index="", header="")
-    cont2=pd.read_csv("BDATOS/contabilidad1.csv")
-    print(cont2.iloc[:,0:5])
-    print(cont2.iloc[:,5:9])
+    cont2.to_csv("BDATOS/contabilidad1.csv", mode="a", index="", header="") # Se graba los nuevos datos a la base del contabilidad
+    cont2=pd.read_csv("BDATOS/contabilidad1.csv")                           # Se lee la base de datos de contabilidad
+    print(cont2.iloc[:,0:5])                                                # Se imprime la primer parte de los datos de la base contabilidad
+    print(cont2.iloc[:,5:9])                                                # Se imprime la segunad parte de los dtos de la base contabilidad
     break
 
 
@@ -326,21 +326,20 @@ def contabilidad():       # se crea la función que controla toda la contabilida
 
 
     
-#print("\t\t\t\t PÁGINA PRINCIPAL DE CONTABILIDAD \n\n")                  # se imprime el titulo de la página del menu del manejo operativo del negocio
+print("\t\t\t\t PÁGINA PRINCIPAL DE CONTABILIDAD \n\n")    # se imprime el titulo de la página del menu del manejo operativo del negocio
 
 
-def menu():                                              # se crea un menu para el manejo operativo del negocio
+def menu():                                                # se crea un menu para el manejo operativo del negocio
     
-    print("\n\n\n\nSelecciona una opción \n")                    # se imprime mendsaje del menú
-    print("\t7.1 - Costos de Bodega completa ")                  # se imprime registro de compras
-    print("\t7.2 - Costos de bodega por fechas")              # se imprime registro total acumulado
-    print("\t7.3 - Ingreso de datos para costos fijos")  
-    print("\t7.4 - Costos fijos completos") 
-    print("\t7.5 - Costos fijos por fecha") 
-    print("\t7.6 - Ingresos por ventas - completa") 
-    print("\t7.7 - Ingresos por ventas - manejo por fechas")
-    print("\t7.8 - CONTABILIDAD")
-    print("\t3 - Vencimiento de productos")            # se imprime ingreso abodega
+    print("\n\n\n\nSelecciona una opción \n")              # se imprime mensaje del menú
+    print("\t7.1 - Costos de Bodega completa ")            # se imprime registro de costos de bodega forma completa
+    print("\t7.2 - Costos de bodega por fechas")           # se imprime registro de costos de bodega forma fechas específicas
+    print("\t7.3 - Ingreso de datos para costos fijos")    # se imprime opción para anexar datos de costsos fijos
+    print("\t7.4 - Costos fijos completos")                # se imprime el registro de costos fijos la base completa
+    print("\t7.5 - Costos fijos por fecha")                # se imprme el registro  de costos fijos la base por fecha específica
+    print("\t7.6 - Ingresos por ventas - completa")        # se imprime el registro de ventas la base completa
+    print("\t7.7 - Ingresos por ventas - manejo por fechas") # Se imprime el registro de ventas la base por fecha específica
+    print("\t7.8 - CONTABILIDAD")                          # se imprime la opción del control toal de la contabilidad
     print("\t9 - Terminar con uso de menu")              # se imprime finalizacón de menú
           
 # Se crea un una iteración con while mientras se cumpla la condición
@@ -348,39 +347,39 @@ while True:                                              # se activa el ciclo wh
       menu()                                             # se activa la función menú
       opcionMenu = input("\n Inserte el número de la opción: ")   # sirve para ingresar el valor de la opción seleccionada
                   
-      if opcionMenu == "7.1":                              # Para activar la función compra
+      if opcionMenu == "7.1":                            # Para activar la función costos de bodega forma completa
           print("\n\nhola\n\n")                          # imprime mensaje de saludo
-          bodegacom()                                     # la función compra se ejecuta
+          bodegacom()                                    # la función costos de bodega de forma completa se ejecuta
      
            
 
         
-      #elif opcionMenu == "7.2":                            # Para activar la función total
-             #bodegafecha()                                     # se ejecuta la función total
+      elif opcionMenu == "7.2":                          # Para activar la función costos de bodega forma por fechas específicas
+             bodegafecha()                               # la función costos de bodega de forma por fechas específicas se ejecuta
       
 
-     # elif opcionMenu == "7.3":                            # se activa la función bodega
-            # ingcostosf()
+      elif opcionMenu == "7.3":                          # se activa la función ingresos de datos para costos fijos
+             ingcostosf()                                # Se ejecuta la función ingresos de datos para costos fijos
         
-      #elif opcionMenu == "7.4":                            # se activa la función bodega
-           # costosf()
+      elif opcionMenu == "7.4":                          # se activa la función costos fijos forma completa
+            costosf()                                    # Se ejecuta la función costos fijos forma completa
 
-      #elif opcionMenu == "7.5":                            # se activa la función bodega
-             #costosff()
+      elif opcionMenu == "7.5":                          # se activa la función costos fijos forma fecha específica
+             costosff()                                  # Se ejecuta la función costos fijos forma fecha específica
 
 
-      #elif opcionMenu == "7.6":                            # se activa la función bodega
-             #ingventasT()
+      elif opcionMenu == "7.6":                          # se activa la función ventas en forma completa
+             ingventasT()                                # se ejecuta la función ventas en forma fechas específica
 
-      #elif opcionMenu == "7.7":                            # se activa la función bodega
-            # ingventasTF()   
+      elif opcionMenu == "7.7":                          # se activa la función ventas en forma fechas específica
+              ingventasTF()                              # se ejecuta la función ventas en forma fechas específica
 
-      elif opcionMenu == "7.8":                            # se activa la función bodega
-             contabilidad()   
+      elif opcionMenu == "7.8":                          # se activa la función contabilidad total del negocio
+             contabilidad()                              # se ejecuta la función contabilidad total del negocio
 
-      #elif opcionMenu == "9":                            # se elige la activa la opción finalizar menú
-          #print("\n\n\t\tSistema finalizado")                    # mensaje que confirma la finalización del menu
-         # break                                          # se finaliza el ciclo if del menú
+      #elif opcionMenu == "9":                           # se elige la activa la opción finalizar menú
+          #print("\n\n\t\tSistema finalizado")           # mensaje que confirma la finalización del menu
+         # break                                         # se finaliza el ciclo if del menú
             
 
 
