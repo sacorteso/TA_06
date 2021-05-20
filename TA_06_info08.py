e40969339cfc27321f608f465f5d60464089bf18
JUAN CARLOS VELÁSQUEZ GUTIÉRREZ <>
2021-05-20 02:33:30 +0000

    Se activa informe 8 y se crea archivo proveedores  para base de datos

---
+++ TA_06_info08.py
@@ -0,0 +1,9 @@
+from datetime import date as dt #librería para manejar fechas
+from datetime import datetime as dtt  #librería para manejar el tiempo
+from datetime import timedelta        #librearía para realizar operaciones matemáticas con el tiempo
+import pandas as pd # Librería para utilizar y manipular dataframe
+import numpy as np  # Librería para realizar operaciones matemáticas y manejo de matrices
+import os           # Librerías para realizar funciones internas del software
+import csv          # Librería para manipular archivos con extensión csv
+import time         # Librería para manipular el tiempo
+
